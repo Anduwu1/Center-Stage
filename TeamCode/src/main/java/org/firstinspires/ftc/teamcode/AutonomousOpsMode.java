661c2b38221c782afdd6f28e3232e9a50ca7ff73
Electro-Corp <chinmaytanmay@outlook.com>
2023-09-11 19:34:13 -0600

    Camera initlization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpsMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpsMode.java
@@ -12,10 +12,27 @@ import org.openftc.easyopencv.OpenCvCameraRotation;
 
 @Config
 @Autonomous(group = "drive")
-public class AutonomousOpsMode extends LinearOpMode {
+public class AutonomousOpsMode extends LinearOpMode implements OpenCvCamera.AsyncCameraOpenListener{
+
+    // OpenCV Camera
+    private OpenCvCamera camera;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
+        // Grab the webcam and init it
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "webcam");
+
+        // Create live preview
+        camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+        camera.openCameraDeviceAsync(this);
+        // [TODO] set a pipeline here
+
+        while(!isStarted()){
+            // Do something
+        }
+
+        // Start autonomus
     }
 }
