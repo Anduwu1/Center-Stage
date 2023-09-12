23eb0bee2f74ffcd04dd14fe366b09d021ff2306
Electro-Corp <chinmaytanmay@outlook.com>
2023-09-11 19:31:11 -0600

    Create AutonomousOpsMode.java
    Basic

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpsMode.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+// OpenCV
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+@Config
+@Autonomous(group = "drive")
+public class AutonomousOpsMode extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
