9b07a9271977b409fa42692df40b09a79ea1a3d8
Androo <andrew.coon84@gmail>
2023-09-27 19:20:54 -0600

    added basic apriltag stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTag.java
@@ -1,15 +1,29 @@
 package org.firstinspires.ftc.teamcode;
 
 public class AprilTag {
+    private int index;
+    private float x, y;
+    private float d, dr, dx, dy;
+    public AprilTag(int _index, float _x, float _y){
+        this.index = _index;
+        this.x = _x;
+        this.y = _y;
+    }
 
-    public AprilTag(int index, float x, float y){
+    public float getX() {
+        return x;
+    }
 
+    public float getY() {
+        return y;
     }
 
     public Point calculate(float range, float bearing, float yaw){
-        float x = this.x - range * Math.cos(bearing);
-        float y = this.y - range * Math.sin(bearing);
+        float _x = this.x - range * Math.cos(bearing);
+        float _y = this.y - range * Math.sin(bearing);
 
-        return new Point(x,y);
+        return new Point(_x,_y);
     }
+
+
 }
