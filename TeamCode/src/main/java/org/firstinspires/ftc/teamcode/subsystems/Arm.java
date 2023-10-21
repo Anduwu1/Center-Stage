38052e0e671ec6a880b9c4ea260b7711fd4af12b
Electro-Corp <chinmaytanmay@outlook.com>
2023-10-21 11:50:47 -0600

    move servo defs to subsystems

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class Arm {
     public static final String arm = "servoA";
+    public Servo armServo = null;
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Bucket.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Bucket.java
@@ -1,6 +1,11 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class Bucket {
     public static final String ROTATION_SERVO = "servoR";
     public static final String TRAPDOOR_SERVO = "servoB";
+
+    public Servo bucketServoOne = null;
+    public Servo bucketServoTwo = null;
 }
