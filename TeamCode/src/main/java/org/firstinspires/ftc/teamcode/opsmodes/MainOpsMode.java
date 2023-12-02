7a85fbe7220b234cc9e16e30e803f5316d13d73b
Androo <84476053+Anduwu1@users.noreply.github.com>
2023-12-01 18:02:47 -0700

    changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opsmodes/MainOpsMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opsmodes/MainOpsMode.java
@@ -6,25 +6,30 @@ import static org.firstinspires.ftc.teamcode.subsystems.Arm.ARM_UP;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.objects.Robot;
 import org.firstinspires.ftc.teamcode.objects.RobotSettings;
 import org.firstinspires.ftc.teamcode.resources.HardwareController;
+import org.firstinspires.ftc.teamcode.roadrunner.drive.DriveConstants;
 import org.firstinspires.ftc.teamcode.subsystems.Arm;
 import org.firstinspires.ftc.teamcode.subsystems.Bucket;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
 @TeleOp(name="Main OpMode")
 public class MainOpsMode extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
 
     // Motors
-    private DcMotor leftFrontDrive = null;
-    private DcMotor leftBackDrive = null;
-    private DcMotor rightFrontDrive = null;
-    private DcMotor rightBackDrive = null;
-
+    private DcMotorEx leftFrontDrive = null;
+    private DcMotorEx leftBackDrive = null;
+    private DcMotorEx rightFrontDrive = null;
+    private DcMotorEx rightBackDrive = null;
     private DcMotor intakeDrive = null;
     private enum ArmState {
         UP,
@@ -139,10 +144,16 @@ public class MainOpsMode extends LinearOpMode {
         hardwareController = new HardwareController(hardwareMap);
 
         // Init hardware Vars
-        leftFrontDrive = hardwareMap.get(DcMotor.class, RobotSettings.BANA_LFDRIVE_MOTOR);
-        leftBackDrive = hardwareMap.get(DcMotor.class, RobotSettings.BANA_LBDRIVE_MOTOR);
-        rightFrontDrive = hardwareMap.get(DcMotor.class, RobotSettings.BANA_RFDRIVE_MOTOR);
-        rightBackDrive = hardwareMap.get(DcMotor.class, RobotSettings.BANA_RBDRIVE_MOTOR);
+        leftFrontDrive = hardwareMap.get(DcMotorEx.class, RobotSettings.BANA_LFDRIVE_MOTOR);
+        leftBackDrive = hardwareMap.get(DcMotorEx.class, RobotSettings.BANA_LBDRIVE_MOTOR);
+        rightFrontDrive = hardwareMap.get(DcMotorEx.class, RobotSettings.BANA_RFDRIVE_MOTOR);
+        rightBackDrive = hardwareMap.get(DcMotorEx.class, RobotSettings.BANA_RBDRIVE_MOTOR);
+        List<DcMotorEx> motors = Arrays.asList(leftFrontDrive, leftBackDrive, rightFrontDrive, rightBackDrive);
+
+        //set mode
+        for (DcMotorEx motor : motors) {
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
 
         intakeDrive = hardwareMap.get(DcMotor.class, "intake");
 
@@ -165,27 +176,39 @@ public class MainOpsMode extends LinearOpMode {
             updateDriveMotors();
             updateServos();
 
-            telemetry.addData("Arm Open %", "%f", armX / (ARM_UP - ARM_DOWN));
-            telemetry.addData("Intake Locked", trapDoor);
-            telemetry.addData("Bucket %", "%f", bucketX - 0.19f / (1.0f - 0.19f));
+            String motorData = String.format(
+                    Locale.US,
+                    "Left Front: %f%n" +
+                            "Left Rear: %f%n" +
+                            "Right Front: %f%n" +
+                            "Right Rear: %f%n",
+                    DriveConstants.encoderTicksToInches(leftFrontDrive.getVelocity()), DriveConstants.encoderTicksToInches(leftBackDrive.getVelocity()), DriveConstants.encoderTicksToInches(rightFrontDrive.getVelocity()), DriveConstants.encoderTicksToInches(rightBackDrive.getVelocity())
+
+            );
+
+
+            // telemetry.addData("Arm Open %", "%f", armX / (ARM_UP - ARM_DOWN));
+            // telemetry.addData("Intake Locked", trapDoor);
+            // telemetry.addData("Bucket %", "%f", bucketX - 0.19f / (1.0f - 0.19f));
             telemetry.addLine("left distance: " + hardwareController.getLeftDistance());
             telemetry.addLine("right distance: " + hardwareController.getRightDistance());
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addLine(motorData);
+            // telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.update();
         }
     }
 
     // Booleans that store if the buttons are pressed
-    boolean rbPressed, bPressed, yPressed = false;
+    boolean rbPressed, bPressed, yPressed, xPressed = false;
 
     boolean open = true, trapDoor = false;
-    private float armX = (float) ARM_DOWN;
+    private float armX = ARM_DOWN;
     private void updateServos(){
         // Trapdoor toggle
         if(gamepad2.y && !yPressed) {
             yPressed = true;
             toggleTrapdoor();
-            telemetry.addLine("Toggling trapdoor");
+            // telemetry.addLine("Toggling trapdoor");
         } else if(!gamepad2.y) {
             yPressed = false;
             //telemetry.addLine("Button Pressed but not toggling");
@@ -200,14 +223,21 @@ public class MainOpsMode extends LinearOpMode {
         }
 
         // Arm Toggle
-        if (gamepad2.right_bumper && !rbPressed) { rbPressed = true; toggleArm(); } else if (!gamepad2.right_bumper){ rbPressed = false; }
+        if (gamepad2.right_bumper && !rbPressed) {
+            rbPressed = true; toggleArm();
+        }
+        else if (!gamepad2.right_bumper){
+            rbPressed = false;
+        }
 
         // Manual arm control
         armX -= gamepad2.left_stick_y / 800.0f;
 
         // Arm clamp
-        if(armX > ARM_UP) armX = (float) ARM_UP;
-        if(armX < ARM_DOWN) armX = (float) ARM_DOWN;
+        if(armX > ARM_UP)
+            armX = ARM_UP;
+        if(armX < ARM_DOWN)
+            armX = ARM_DOWN;
 
         // Arm location percenetage for telemetry
         // float percent = (float) ((armX - Arm.ARM_DOWN) / (Arm.ARM_UP - Arm.ARM_DOWN));
@@ -217,8 +247,10 @@ public class MainOpsMode extends LinearOpMode {
         if(gamepad2.dpad_down) bucketX-=0.005f;
 
         // Clamps the bucket servo position
-        if(bucketX > Bucket.DROP_POS) bucketX = Bucket.DROP_POS;
-        if(bucketX < Bucket.INTAKE_POS) bucketX = Bucket.INTAKE_POS;
+        if(bucketX > Bucket.DROP_POS)
+            bucketX = Bucket.DROP_POS;
+        if(bucketX < Bucket.INTAKE_POS)
+            bucketX = Bucket.INTAKE_POS;
 
         // Updates servos
         hardwareController.servoMove(bucketX, HardwareController.Servo_Type.BUCKET_SERVO);
@@ -230,11 +262,19 @@ public class MainOpsMode extends LinearOpMode {
     private void updateDriveMotors() {
         // Max motor speed
         double max;
+        double min;
+
+        double axial = 0;
+        double lateral = 0;
+        double yaw = 0;
 
         // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-        double axial = -gamepad1.left_stick_y;
-        double lateral = gamepad1.left_stick_x;
-        double yaw = gamepad1.right_stick_x;
+        if (Math.abs(gamepad1.left_stick_y) > 0.05)
+            axial = -gamepad1.left_stick_y;
+        if (Math.abs(gamepad1.left_stick_x) > 0.05)
+            lateral = gamepad1.left_stick_x;
+        if (Math.abs(gamepad1.right_stick_x) > 0.05)
+            yaw = gamepad1.right_stick_x;
 
         // axial and yaw lock
         boolean aylock = false;
@@ -254,9 +294,9 @@ public class MainOpsMode extends LinearOpMode {
 
         // Bumpers Turn Bot Left and Right
         if (gamepad1.left_bumper)
-            yaw += 0.6 * -1;
+            yaw += 0.3 * -1;
         if (gamepad1.right_bumper)
-            yaw -= 0.6 * -1;
+            yaw -= 0.3 * -1;
 
 //        // Auto alignment
 //        if (gamepad1.a && !aPressed && aylock) {
@@ -277,7 +317,7 @@ public class MainOpsMode extends LinearOpMode {
             if (Math.abs(hardwareController.getLeftDistance() - hardwareController.getRightDistance()) > 0.05) {
                 yaw = -(hardwareController.getLeftDistance() - hardwareController.getRightDistance()) / (hardwareController.getLeftDistance() * 7);
                 aylock = false;
-                if (hardwareController.getLeftDistance() >= 0.5 || hardwareController.getRightDistance() >= 0.5) {
+                if (hardwareController.getLeftDistance() >= 2 || hardwareController.getRightDistance() >= 2) {
                     axial = -1 * ((hardwareController.getLeftDistance() + hardwareController.getRightDistance()) / 2) / 42;
                     if (axial < -1)
                         axial = -1;
@@ -286,9 +326,17 @@ public class MainOpsMode extends LinearOpMode {
             }
         }
 
-        // locks the movement of anything but lateral if robot is aligned wi    th backdrop
+        // can turn on a mode to protect from banging into the backdrop
+        if(gamepad2.x && !xPressed) {
+            xPressed = true;
+            aylock = !aylock;
+        } else if (!gamepad2.x) {
+            xPressed = false;
+        }
+
+        // locks the movement of anything but lateral if robot is aligned with backdrop
         if (aylock) {
-            axial = 0;
+            axial = -1 * Math.abs(axial);
             yaw = 0;
         }
 
@@ -304,11 +352,20 @@ public class MainOpsMode extends LinearOpMode {
         leftBackPower *= DRIVE_SPEED;
         rightBackPower *= DRIVE_SPEED;
 
+        double leftFrontEx = DriveConstants.encoderTicksToInches(leftFrontDrive.getVelocity());
+        double rightFrontEx = DriveConstants.encoderTicksToInches(rightFrontDrive.getVelocity());
+        double leftBackEx = DriveConstants.encoderTicksToInches(leftBackDrive.getVelocity());
+        double rightBackEx = DriveConstants.encoderTicksToInches(rightBackDrive.getVelocity());
+
         // Normalize the values so no wheel power exceeds 100%
         // This ensures that the robot maintains the desired motion.
         max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
         max = Math.max(max, Math.abs(leftBackPower));
         max = Math.max(max, Math.abs(rightBackPower));
+
+        min = Math.min(leftFrontEx * leftFrontPower, rightFrontEx * rightFrontPower);
+        min = Math.min(min, rightBackEx * rightBackPower);
+        min = Math.min(min, leftBackEx * leftBackPower);
 
         if (max > 1.0) {
             leftFrontPower /= max;
@@ -317,11 +374,28 @@ public class MainOpsMode extends LinearOpMode {
             rightBackPower /= max;
         }
 
+        double lro = leftBackEx / min;
+        double lfo = leftFrontEx / min;
+        double rro = rightBackEx / min;
+        double rfo = rightFrontEx / min;
+
+        if (lro == 0)
+            lro = 1;
+        if (rro == 0)
+            rro = 1;
+        if (lfo == 0)
+            lfo = 1;
+        if (rfo == 0)
+            rfo = 1;
+
         // Send calculated power to wheels
-        leftFrontDrive.setPower(leftFrontPower);
-        rightFrontDrive.setPower(rightFrontPower);
-        leftBackDrive.setPower(leftBackPower);
-        rightBackDrive.setPower(rightBackPower);
+        // leftFrontDrive.setPower(leftFrontPower * .98);
+        leftFrontDrive.setPower(leftFrontPower /*/ lfo*/  * .96);
+        rightFrontDrive.setPower(rightFrontPower /*/ rfo*/);
+        leftBackDrive.setPower(leftBackPower /*/ lro*/);
+        rightBackDrive.setPower(rightBackPower /*/ rro*/ * .85);
+
+        // rightBackDrive.setPower(rightBackPower * .82);
 
         // Intake
         intakePower = 0;
