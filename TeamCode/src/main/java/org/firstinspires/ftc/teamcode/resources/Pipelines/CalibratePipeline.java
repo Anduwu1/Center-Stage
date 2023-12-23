d79fe19e01b34cc47bcce59077b07149389d2eda
Androo <84476053+Anduwu1@users.noreply.github.com>
2023-12-23 11:49:55 -0700

    put hsv values as text onto camera output

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/resources/Pipelines/CalibratePipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/resources/Pipelines/CalibratePipeline.java
@@ -2,10 +2,13 @@ package org.firstinspires.ftc.teamcode.resources.Pipelines;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.Locale;
 
 public class CalibratePipeline extends OpenCvPipeline {
 
@@ -22,6 +25,20 @@ public class CalibratePipeline extends OpenCvPipeline {
         Rect rect = new Rect(((input.width() - size) / 2), (input.height() - size) / 2, size, size);
         Imgproc.rectangle(input, rect, new Scalar(0, 255, 0), 1, 8, 0);
         values = hsvMat.get(input.height() / 2, input.width() / 2);
+        Imgproc.putText(
+                input,
+                String.format(
+                        Locale.US,
+                        "HSV, (%f, %f, %f)",
+                        values[0], values[1], values[2]
+                ),
+                new Point(input.width() / 2, input.height()/2 - size),
+                Imgproc.FONT_HERSHEY_SIMPLEX,
+                10.0,
+                new Scalar(0, 0, 0),
+                1
+        );
+
         return input;
     }
 
